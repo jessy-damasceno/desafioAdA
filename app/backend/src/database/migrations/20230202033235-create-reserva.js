3ff2ed4584e03121c1d056657b447a2a00578712
Jessy Damasceno <jessy_bas@hotmail.com>
2023-02-02 00:47:49 -0300

    feat(migrations)!: create sequelize migrations

---
+++ app/backend/src/database/migrations/20230202033235-create-reserva.js
@@ -0,0 +1,70 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("reservas", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      checkIn: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      checkOut: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      rentPrice: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      rentTax: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      property: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "propriedades",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      extrasPrice: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      extrasTax: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      total: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      payment: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pending: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      portal: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable("users");
+  },
+};
---
+++ app/backend/src/database/migrations/20230202033726-create-conta.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      property: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "propriedades",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      price: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      dueDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      commission: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
