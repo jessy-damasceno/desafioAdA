ef6d4851e7f6f8e43e46a303dfe19384fa21f503
Jessy Damasceno <jessy_bas@hotmail.com>
2023-02-02 03:34:50 -0300

    refatore[database.ts]: remove unnecessary timezone

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -8,9 +8,6 @@ const config: Options = {
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3002,
   dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
   logging: false,
 }
 
